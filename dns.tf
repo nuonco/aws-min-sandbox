d37350e706f226033c318d8e5013116dd9da54c0
Mark Milligan <mark@nuon.co>
2025-08-21 20:43:40 -0500

    updated to accomodate non-eks apps

--- dns.tf
+++ dns.tf
@@ -1,20 +1,23 @@
 resource "aws_route53_zone" "internal" {
-  name = var.internal_root_domain
-
+  name          = var.internal_root_domain
   force_destroy = true
+
   vpc {
     vpc_id = var.vpc_id
   }
 }
 
 resource "aws_route53_zone" "public" {
-  name = var.public_root_domain
+  count = var.enable_nuon_dns ? 1 : 0
 
+  name          = var.public_root_domain
   force_destroy = true
 }
 
 resource "aws_route53_record" "caa" {
-  zone_id = aws_route53_zone.public.zone_id
+  count = var.enable_nuon_dns ? 1 : 0
+
+  zone_id = aws_route53_zone.public[0].zone_id
   name    = var.public_root_domain
   type    = "CAA"
   ttl     = 300
